6b9ada80550fb449f6f184494349915f585c965b
Toshan-Rijal <97708309+Toshan-Rijal@users.noreply.github.com>
2022-01-13 16:46:07 -0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Toshan-Rijal
+- 👀 I’m interested in solving Rubik's Cubes.
+- 🌱 I’m currently learning Python.
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me Gmail.
+
+<!---
+Toshan-Rijal/Toshan-Rijal is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
